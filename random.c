0b1864e87258e395cc24db1df3f0f610d4f00d9b
0AndyZhou0 <azhou4@stuy.edu>
2018-11-19 19:44:56 -0500

    finally remembered to finish it because of threates

--- random.c
+++ random.c
@@ -7,7 +7,7 @@ int randgen(){
   int file = open("/dev/random", O_RDONLY);
   int *number = (int *)malloc(sizeof(int));
   read(file,number,sizeof(int));
-  printf("%d\n", *number);
+  //printf("%d\n", *number);
   int num = *number;
   free(number);
   if(close(file) < 0){
@@ -18,5 +18,32 @@ int randgen(){
 }
 
 int main(){
-  return randgen();
+  int file = open("nums.txt", O_RDWR | O_CREAT, 0666);
+  int* nums = (int*)malloc(10*sizeof(int));
+  for(int i = 0;i < 10;i++){
+    nums[i] = randgen();
+  }
+  printf("\n");
+  for(int i = 0;i < 10;i++){
+    printf("%d\n",nums[i]);
+  }
+  write(file,&nums,sizeof(*nums));
+  /*
+  for(int i = 0;i < 10;i++){
+    write(file,&nums[i],sizeof(int));
+  }
+  */
+  file = open("nums.txt", O_RDWR);
+  int* copy = (int*)malloc(10*sizeof(int));
+  read(file,&copy,sizeof(*copy));
+  /*
+  for(int i = 0;i < 10;i++){
+    read(file,&copy[i],sizeof(int));
+  }
+  */
+  printf("\n");
+  for(int i = 0;i < 10;i++){
+    printf("%d\n",copy[i]);
+  }
+  return 0;
 }
